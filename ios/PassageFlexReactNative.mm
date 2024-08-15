e0f1da8bb4bb6db51aafe2774b7228d9f36a7732
Ricky Padilla <rickycpadilla@gmail.com>
2024-08-15 13:30:33 -0600

    feat(PSG-4521): iOS dependencies and bridge code

--- ios/PassageFlexReactNative.mm
+++ ios/PassageFlexReactNative.mm
@@ -6,6 +6,14 @@ RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
+RCT_EXTERN_METHOD(register:(float)transactionId
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(authenticate:(float)transactionId
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
 + (BOOL)requiresMainQueueSetup
 {
   return NO;
--- ios/PassageFlexReactNative.swift
+++ ios/PassageFlexReactNative.swift
@@ -1,8 +1,46 @@
+import PassageFlex
+
 @objc(PassageFlexReactNative)
 class PassageFlexReactNative: NSObject {
-
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
+    
+    @objc(register:withResolver:withRejecter:)
+    func register(
+        transactionId: String,
+        resolve: @escaping RCTPromiseResolveBlock,
+        reject: @escaping RCTPromiseRejectBlock
+    ) -> Void {
+        Task {
+            do {
+                if #available(iOS 16.0, *) {
+                    let nonce = try await PassageFlex.Passkey.register(with: transactionId)
+                    resolve(nonce)
+                } else {
+                    // TODO: throw error
+                }
+            } catch {
+                reject(error.localizedDescription, error.localizedDescription, error)
+            }
+        }
+    }
+    
+    @objc(authenticate:withResolver:withRejecter:)
+    func authenticate(
+        transactionId: String,
+        resolve: @escaping RCTPromiseResolveBlock,
+        reject: @escaping RCTPromiseRejectBlock
+    ) -> Void {
+        Task {
+            do {
+                if #available(iOS 16.0, *) {
+                    let nonce = try await PassageFlex.Passkey.authenticate(with: transactionId)
+                    resolve(nonce)
+                } else {
+                    // TODO: throw error
+                }
+            } catch {
+                reject(error.localizedDescription, error.localizedDescription, error)
+            }
+        }
+    }
+    
 }
