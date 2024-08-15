e37039c70f002f8fe2f3d2d71f2215ec92e856d5
Ricky Padilla <rickycpadilla@gmail.com>
2024-08-15 09:47:16 -0600

    feat(PSG-4522): Android dependency and bridge code

--- android/src/main/java/com/passageflexreactnative/PassageFlexReactNativeModule.kt
+++ android/src/main/java/com/passageflexreactnative/PassageFlexReactNativeModule.kt
@@ -4,6 +4,10 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import id.passage.passageflex.PassageFlex
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class PassageFlexReactNativeModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
@@ -12,14 +16,47 @@ class PassageFlexReactNativeModule(reactContext: ReactApplicationContext) :
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  @ReactMethod
-  fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
-  }
-
   companion object {
     const val NAME = "PassageFlexReactNative"
   }
+
+  @ReactMethod
+  fun register(transactionId: String, promise: Promise) {
+    CoroutineScope(Dispatchers.IO).launch {
+      try {
+        if (currentActivity == null) {
+          // promise.reject(e)
+          return@launch
+        }
+        val nonce = PassageFlex.Passkey.register(
+          transactionId,
+          currentActivity!!
+          // Add authenticator attachment option?
+        )
+        promise.resolve(nonce)
+      } catch (e: Exception) {
+        promise.reject(e)
+      }
+    }
+  }
+
+  @ReactMethod
+  fun authenticate(transactionId: String, promise: Promise) {
+    CoroutineScope(Dispatchers.IO).launch {
+      try {
+        if (currentActivity == null) {
+          // promise.reject(e)
+          return@launch
+        }
+        val nonce = PassageFlex.Passkey.authenticate(
+          transactionId,
+          currentActivity!!
+        )
+        promise.resolve(nonce)
+      } catch (e: Exception) {
+        promise.reject(e)
+      }
+    }
+  }
+
 }
