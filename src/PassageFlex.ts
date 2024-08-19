ca4da724f1c101a035c5dd2ca1c165b5cdf99632
Ricky Padilla <rickycpadilla@gmail.com>
2024-08-19 17:05:53 -0600

    feat(PSG-4520): added PassageFlexPasskey class

---
+++ src/PassageFlex.ts
@@ -0,0 +1,25 @@
+import PassageFlexPasskey from './PassageFlexPasskey';
+import { PassageFlexReactNative } from './PassageFlexReactNativeModule';
+
+/**
+ * The PassageFlex class is used to easily add passkeys to your existing authentication system on Apple devices.
+ *
+ * @example
+ * ```
+ * import PassageFlex from '@passageidentity/passage-flex-react-native';
+ *
+ * const passageFlex = new PassageFlex('MY_APP_ID');
+ * ```
+ */
+class PassageFlex {
+  passkey: PassageFlexPasskey;
+  appId: string;
+
+  constructor(appId: string) {
+    this.appId = appId;
+    this.passkey = new PassageFlexPasskey(appId);
+    PassageFlexReactNative.initWithAppId(appId);
+  }
+}
+
+export default PassageFlex;
