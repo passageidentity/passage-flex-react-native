2d068bf51f006c582bab0e4a90aaab45303e9eaf
Ricky Padilla <rickycpadilla@gmail.com>
2024-08-21 15:26:47 -0600

    chore: cleaned up plugin code

--- src/app.plugin.js
+++ src/app.plugin.js
@@ -8,23 +8,11 @@ const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
 
-// Helper to modify Podfile for iOS
-function addPodfileDependency(contents) {
-  const podDependency = `pod 'PassageFlex', '0.2.0'`;
-
-  if (!contents.includes(podDependency)) {
-    const targetIndex = contents.lastIndexOf(`target '`);
-    if (targetIndex !== -1) {
-      const nextLineIndex = contents.indexOf('\n', targetIndex);
-      contents =
-        contents.slice(0, nextLineIndex) +
-        `  ${podDependency}\n` +
-        contents.slice(nextLineIndex);
-    }
-  }
-
-  return contents;
-}
+// Declare native Flex dependencies
+const flexAndroidVersion = '0.2.0';
+const gradelDependency = `implementation 'id.passage.android:passageflex:${flexAndroidVersion}'`;
+const flexIosVersion = '0.2.0';
+const podDependency = `pod 'PassageFlex', '${flexIosVersion}'`;
 
 // iOS: Add PassageFlex CocoaPod
 function withIosPodfile(configuration) {
@@ -36,11 +24,17 @@ function withIosPodfile(configuration) {
         'Podfile'
       );
       let podfileContents = fs.readFileSync(podfilePath, 'utf-8');
-
-      podfileContents = addPodfileDependency(podfileContents);
-
+      if (!podfileContents.includes(podDependency)) {
+        const targetIndex = podfileContents.lastIndexOf(`target '`);
+        if (targetIndex !== -1) {
+          const nextLineIndex = podfileContents.indexOf('\n', targetIndex);
+          podfileContents =
+            podfileContents.slice(0, nextLineIndex) +
+            `  ${podDependency}\n` +
+            podfileContents.slice(nextLineIndex);
+        }
+      }
       fs.writeFileSync(podfilePath, podfileContents);
-
       return config;
     },
   ]);
@@ -71,10 +65,7 @@ function withAndroidGradleDependency(configuration) {
         'build.gradle'
       );
       let buildGradleContents = fs.readFileSync(buildGradlePath, 'utf-8');
-
-      const passageDependency = `implementation 'id.passage.android:passageflex:0.2.0'`;
-
-      if (!buildGradleContents.includes(passageDependency)) {
+      if (!buildGradleContents.includes(gradelDependency)) {
         const dependenciesIndex = buildGradleContents.indexOf('dependencies {');
         if (dependenciesIndex !== -1) {
           const nextLineIndex = buildGradleContents.indexOf(
@@ -83,13 +74,11 @@ function withAndroidGradleDependency(configuration) {
           );
           buildGradleContents =
             buildGradleContents.slice(0, nextLineIndex + 1) +
-            `    ${passageDependency}\n` +
+            `    ${gradelDependency}\n` +
             buildGradleContents.slice(nextLineIndex + 1);
         }
       }
-
       fs.writeFileSync(buildGradlePath, buildGradleContents);
-
       return config;
     },
   ]);
@@ -105,15 +94,12 @@ function withAndroidAssociatedDomain(configuration) {
   // Modify AndroidManifest.xml
   configuration = withAndroidManifest(configuration, async (config) => {
     const application = config.modResults.manifest.application[0];
-
     if (!application.hasOwnProperty('meta-data')) {
       application['meta-data'] = [];
     }
-
     const metaDataExists = application['meta-data'].some(
       (metaData) => metaData.$['android:name'] === 'asset_statements'
     );
-
     if (!metaDataExists) {
       application['meta-data'].push({
         $: {
@@ -122,7 +108,6 @@ function withAndroidAssociatedDomain(configuration) {
         },
       });
     }
-
     return config;
   });
 
@@ -134,40 +119,29 @@ function withAndroidAssociatedDomain(configuration) {
         config.modRequest.platformProjectRoot,
         'app/src/main/res/values/strings.xml'
       );
-
       if (!fs.existsSync(stringsXmlPath)) {
         throw new Error(`strings.xml not found at ${stringsXmlPath}`);
       }
-
       let stringsXmlContent = fs.readFileSync(stringsXmlPath, 'utf-8');
-
       // Check if the string already exists
       if (!stringsXmlContent.includes('<string name="passage_auth_origin">')) {
-        // Add the new strings to strings.xml
         const newStrings = `<string name="passage_auth_origin">${associatedDomain}</string>
         <string name="asset_statements">[{"include": "https://@string/passage_auth_origin/.well-known/assetlinks.json"}]</string>`;
-
         // Insert the new strings before the closing </resources> tag
         stringsXmlContent = stringsXmlContent.replace(
           '</resources>',
           `${newStrings}\n</resources>`
         );
-
-        // Write the updated contents back to strings.xml
         fs.writeFileSync(stringsXmlPath, stringsXmlContent);
       }
-
       return config;
     },
   ]);
-
   return configuration;
 }
 
-module.exports = withAndroidAssociatedDomain;
-
 // Combine all plugins into a single config plugin
-const withPassage = (configuration) => {
+const passageFlexPlugin = (configuration) => {
   return withPlugins(configuration, [
     withIosPodfile,
     withIosAssociatedDomain,
@@ -176,4 +150,4 @@ const withPassage = (configuration) => {
   ]);
 };
 
-export default withPassage;
+export default passageFlexPlugin;
